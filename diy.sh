dfb234f49385a10c98aee94816f0ef9fa4794171
jiangxi14520 <jiangxi14520@gmail.com>
2020-03-03 11:19:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd package/
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/Lienol/openwrt-package
+./scripts/feeds update -a
+./scripts/feeds install -a
